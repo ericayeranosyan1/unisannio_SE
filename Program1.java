0bea769742113447a9a22013a7a812a3afdb0c54
ericayeranosyan1 <119513520+ericayeranosyan1@users.noreply.github.com>
2023-01-15 23:30:12 +0100

    Add files via upload

---
+++ Program1.java
@@ -0,0 +1,9 @@
+package Semana1;
+public class Program1{
+	public static void main(String [] args) {
+		for(int i = 0; i < args.length; i++){
+			System.out.println(i + ": "+ args[i]);
+		}
+
+	}
+}
